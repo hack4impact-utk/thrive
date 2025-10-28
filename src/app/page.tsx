73110b26506b972f51ac7a69834b5787d379cb6e
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2025-10-27 21:20:12 -0400

    attempt 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import { Box } from "@mui/material";
 import { ReactNode } from "react";
 
-import HomeCard from "@/components/home-card";
-
 export default function HomePage(): ReactNode {
-  return (
-    <Box
-      sx={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <HomeCard />
-    </Box>
-  );
+  return <h1>Empty Page</h1>;
 }
