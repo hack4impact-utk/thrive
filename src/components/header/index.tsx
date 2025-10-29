11cc6a9faf4dc367c8a0c85ce9c2c9c0c1b913e2
Joseph Kifle <kifle.joe@gmail.com>
2025-10-28 21:10:22 -0400

    descriptive message

---
+++ src/components/header/index.tsx
@@ -0,0 +1,41 @@
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+export default function ButtonAppBar(): React.ReactElement {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Button color="inherit">Sign in</Button>
+          <Button color="inherit">Create account</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
