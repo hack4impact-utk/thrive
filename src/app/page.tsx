7151d953f1f60e2a995ab61e81c2116765cb9799
Jalen Brooks <jbrook92@vols.utk.edu>
2025-11-18 01:45:08 -0500

    Fixed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 import { ReactNode } from "react";
 
-<<<<<<< HEAD
-import VolunteerEventCard from "@/components/VolunteerEventCard/index";
-=======
 import VolunteerEventCard from "@/components/VolunteerEventCard";
->>>>>>> 295ecab (feat: Created VolunteerEventCard component that will go at the bottom of the page)
 
 export default function HomePage(): ReactNode {
   return (
