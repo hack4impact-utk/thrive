59b91627e758f1b4f0184a73d17a1e9cccae98ce
Jalen Brooks <jbrook92@vols.utk.edu>
2025-11-18 01:21:41 -0500

    feat: Created VolunteerEventCard component that will go at the bottom of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 import { ReactNode } from "react";
 
+<<<<<<< HEAD
 import VolunteerEventCard from "@/components/VolunteerEventCard/index";
+=======
+import VolunteerEventCard from "@/components/VolunteerEventCard";
+>>>>>>> 295ecab (feat: Created VolunteerEventCard component that will go at the bottom of the page)
 
 export default function HomePage(): ReactNode {
   return (
