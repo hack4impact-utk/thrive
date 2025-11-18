35a256579305326461fc78b0bb69db02d822c45c
Jalen Brooks <jbrook92@vols.utk.edu>
2025-11-17 23:35:54 -0500

    Created VolunteerEventCard component that goes at the bottom of page

--- src/components/VolunteerEventCard/index.tsx
+++ src/components/VolunteerEventCard/index.tsx
@@ -4,7 +4,6 @@ import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
