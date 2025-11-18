1e663bbc3293c1d6c7faba03bee034894e389d68
marker-sss <116760372+marker-sss@users.noreply.github.com>
2025-11-18 14:38:08 -0500

    feat: page 4/4 of account sign up card (#7)
    Co-authored-by: Agustin Salas-Vaca <123993722+AgustinSV@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import { ReactNode } from "react";
 
 import VolunteerEventCard from "@/components/VolunteerEventCard";
+import OutlinedCard from "@/components/account-creation/index";
 
 export default function HomePage(): ReactNode {
   return (
     <div>
       <h1>hello</h1>
       <VolunteerEventCard />
+      <OutlinedCard />
     </div>
   );
 }
