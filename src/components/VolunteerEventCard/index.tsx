4569f730cb3a26505b516b832068cc98a4cdc3ab
jalenb947 <jalenb947@gmail.com>
2025-11-18 14:30:17 -0500

    feat: Created Volunteer event card component (#9)
    Co-authored-by: Jalen Brooks <jbrook92@vols.utk.edu>

---
+++ src/components/VolunteerEventCard/index.tsx
@@ -0,0 +1,88 @@
+import LocationPinIcon from "@mui/icons-material/LocationPin";
+import PersonIcon from "@mui/icons-material/Person";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+const card = (
+  <React.Fragment>
+    <Box
+      sx={{
+        backgroundColor: "#000080",
+        color: "white",
+        px: 2,
+        py: 1.5,
+        borderTopLeftRadius: "6px",
+        borderTopRightRadius: "6px",
+      }}
+    >
+      <Typography variant="h6" sx={{ fontWeight: 600 }}>
+        Monday, October 20, 2025
+      </Typography>
+    </Box>
+    <CardContent>
+      <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+          <Typography variant="h6" component="div">
+            Main Campus All Day Volunteer
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 0.5, ml: 120 }}>
+          <Button size="small" variant="contained">
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+          <QueryBuilderIcon sx={{ fontSize: 18, color: "text.secondary" }} />
+          <Typography sx={{ color: "text.secondary", fontSize: 14 }}>
+            Mon, October 20 at 3 PM - 6 PM (3 Hours)
+          </Typography>
+        </Box>
+
+        <Box sx={{ display: "flex", alignItems: "center", gap: 0.5, ml: 20 }}>
+          <PersonIcon sx={{ fontSize: 18, color: "text.secondary" }} />
+          <Typography sx={{ color: "text.secondary", fontSize: 14 }}>
+            5 slots remaining
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 0.5, mb: 1 }}>
+        <LocationPinIcon
+          sx={{
+            fontSize: 18,
+            color: "text.secondary",
+          }}
+        />
+        <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+          1317 Connecticut Avenue Knoxville Tennessee 37921
+        </Typography>
+      </Box>
+      <Typography variant="body2">
+        Join us for a fun-filled day of worship, Bible study, academic time,
+        group games, and so much more!
+      </Typography>
+    </CardContent>
+  </React.Fragment>
+);
+
+export default function VolunteerEventCard(): React.ReactElement {
+  return (
+    <Box
+      sx={{
+        minHeight: "80vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        minWidth: 275,
+      }}
+    >
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
+}
