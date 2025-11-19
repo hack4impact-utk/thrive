a4f9e95ba0651fe407eb1304c816ce64f8484e7a
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2025-11-19 12:37:51 -0500

    bug: quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { ReactNode } from "react";
 
-import OutlinedCard from "@/components/account-creation/index";
-import SignUpCard from "@/components/sign-up-card";
-import VolunteerEventCard from "@/components/VolunteerEventCard";
-
 export default function HomePage(): ReactNode {
   return (
     <div>
-      <OutlinedCard />
+      <h1>Hello</h1>
     </div>
   );
 }
