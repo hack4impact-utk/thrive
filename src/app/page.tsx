01439e3201d15e0a548748b6687dd5a6e1882ece
Mithun Titus Maria Ruban Raj <mrrmithuntitus@gmail.com>
2025-11-19 00:05:22 -0500

    Titus questions component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 
-import BasicTextFields from "@/components/text-field/index";
+import BasicQuestionComponent from "@/components/text-field/index";
 
 export default function HomePage(): ReactNode {
   return (
     <div>
       <h1>Empty Page</h1>
-      <BasicTextFields />
+      <BasicQuestionComponent />
     </div>
   );
 }
