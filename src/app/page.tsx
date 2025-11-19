bb6e0959aa2a9d0b7e12c3c2d5e53727fe83620b
Agustin Salas-Vaca <123993722+AgustinSV@users.noreply.github.com>
2025-11-19 14:25:07 -0500

    main page connected (#14)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 "use client";
+import Box from "@mui/material/Box";
 import { ReactNode } from "react";
+
+import VolunteerEventCard from "@/components/VolunteerEventCard";
+import VolunteerEventCardHeader from "@/components/VolunteerEventCardHeader";
+import WelcomeCard from "@/components/WelcomeCard";
 
 export default function HomePage(): ReactNode {
   return (
     <div>
-      <h1>Hello</h1>
+      <Box
+        sx={{
+          width: "80%",
+          maxWidth: 1000,
+          mx: "auto",
+          p: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <WelcomeCard />
+        <VolunteerEventCardHeader />
+        <VolunteerEventCard />
+      </Box>
     </div>
   );
 }
