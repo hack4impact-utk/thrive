b9f1bd4131b278dce35971be1bda375317b27c75
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2025-11-19 17:41:39 -0500

    demo root, create-account, create-account-2

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
 import FormControl from "@mui/material/FormControl";
+import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import Input from "@mui/material/Input";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -17,11 +19,58 @@ export default function CreateAccountPage(): React.ReactElement {
     <Box
       sx={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         paddingTop: 5,
       }}
     >
+      <CardContent sx={{ bgcolor: "#f0f0f0ff", boxShadow: 3, width: "80%" }}>
+        <CardHeader
+          title="Account"
+          sx={{
+            bgcolor: "primary.secondary",
+            color: "secondary.main",
+            textAlign: "center",
+          }}
+        ></CardHeader>
+        <Grid
+          sx={{
+            width: "80%",
+            alignContent: "center",
+            margin: "auto",
+            borderRadius: 2,
+            border: 1,
+          }}
+          container
+          spacing={0}
+        >
+          <Grid sx={{ width: "50%" }}>
+            <Box
+              component="section"
+              sx={{
+                bgcolor: "secondary.main",
+                p: 1.5,
+                width: "100%",
+                borderTopLeftRadius: 8,
+                borderBottomLeftRadius: 8,
+              }}
+            ></Box>
+          </Grid>
+          <Grid sx={{ width: "50%" }}>
+            <Box
+              component="section"
+              sx={{
+                bgcolor: "white",
+                p: 1.5,
+                width: "100%",
+                borderTopRightRadius: 8,
+                borderBottomRightRadius: 8,
+              }}
+            ></Box>
+          </Grid>
+        </Grid>
+      </CardContent>
       <Card
         sx={{
           display: "flex",
@@ -96,6 +145,20 @@ export default function CreateAccountPage(): React.ReactElement {
               }
             />
           </FormControl>
+          <Box
+            sx={{
+              bgcolor: "#22A27E",
+              color: "#FFFFFF",
+              borderRadius: 1,
+              display: "flex",
+              justifyContent: "center",
+              width: "fit-content",
+              marginTop: 2,
+              marginLeft: 1,
+            }}
+          >
+            <Button color="inherit">Next</Button>
+          </Box>
         </CardContent>
       </Card>
     </Box>
