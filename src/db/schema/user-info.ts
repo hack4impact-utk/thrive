7fd64e2f8e1487cdcd1a74013113ad01f3cf1d53
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2026-01-23 23:59:27 -0500

    creating schema for users info

---
+++ src/db/schema/user-info.ts
@@ -0,0 +1,13 @@
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const userInfo = pgTable(
+  "user_info",
+  {
+    username:
+    salutations:
+    firstName:
+    middleName:
+    lastName:
+
+  }
+)
