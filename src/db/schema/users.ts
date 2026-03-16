3c0bcaadef70c0a5b32fbf8f627826deaed9a239
AgustinSV <agustin1salasvaca@gmail.com>
2026-03-16 16:06:47 -0400

    added info_filled column to users table

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("user", {
   id: text("id")
@@ -7,6 +7,7 @@ export const users = pgTable("user", {
   role: text("role").notNull().default("user"),
   name: text("name"),
   email: text("email").unique(),
+  infoFilled: boolean("info_filled").notNull().default(false),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
 });
