b51dc0b3cd15e1a8757b149d60c16b9e21886014
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2026-03-18 00:18:32 -0400

    big update: updated dependencies AND added 'info_filled' and 'role' to session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,34 @@
-import { getServerSession, Session } from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
-import authOptions from "@/app/api/auth/[...nextauth]/auth-options";
+import db from "@/db";
+import { accounts, sessions, users } from "@/db/schema";
 
-export const auth = (): Promise<Session | null> =>
-  getServerSession(authOptions);
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+  }),
+
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+        session.user.infoFilled = user.infoFilled;
+        session.user.role = user.role;
+      }
+      return session;
+    },
+  },
+});
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -6,6 +6,14 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      infoFilled: boolean;
+      role: string;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    infoFilled: boolean;
+    role: string;
   }
 }
