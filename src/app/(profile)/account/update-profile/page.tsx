6f975c6e8c22204786e0d657a826c6da15824bdc
Agustin Salas-Vaca <agustin1salasvaca@gmail.com>
2026-04-26 18:52:54 -0400

    style: clear contents of manage hours and profile page for incoming redesign

--- src/app/(profile)/account/update-profile/page.tsx
+++ src/app/(profile)/account/update-profile/page.tsx
@@ -1,28 +1,5 @@
-import { Box, Typography } from "@mui/material";
 import React from "react";
 
-import PageContainer from "@/components/layout/PageContainer";
-
 export default function UpdateProfile(): React.ReactElement {
-  return (
-    <PageContainer>
-      <Box
-        sx={{
-          backgroundColor: "secondary.main",
-          color: "white",
-          py: 3,
-        }}
-      >
-        <Typography variant="h4" sx={{ fontWeight: 400 }}>
-          Update Profile
-        </Typography>
-      </Box>
-
-      <Box sx={{ color: "black", py: 5 }}>
-        <Typography variant="body1">
-          Update profile will go here later
-        </Typography>
-      </Box>
-    </PageContainer>
-  );
+  return <></>;
 }
